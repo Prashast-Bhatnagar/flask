2076f94273db0a1114d9d6b55728d0afe1eeb75e
Prashast Bhatnagar <physicsbeyondhuman@gmail.com>
2022-01-21 13:25:21 +0530

    init removed

--- app.py
+++ app.py
@@ -20,11 +20,6 @@ class Item(db.Model):
   title = db.Column(db.String(80), unique=True, nullable=False)
   content = db.Column(db.String(120), unique=True, nullable=False)
 
-  def _init_(self, title, content):
-    self.title = title
-    self.content = content
-
-
 db.create_all()
 
 @app.route('/items/<id>', methods=['GET'])
