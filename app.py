82c38176a0f5bd27563193c2ec58cd7b05cd9df8
Prashast Bhatnagar <physicsbeyondhuman@gmail.com>
2022-01-21 14:03:16 +0530

    changes in routes

--- app.py
+++ app.py
@@ -17,11 +17,10 @@ def hello():
 
 class Item(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(80), unique=True, nullable=False)
-  content = db.Column(db.String(120), unique=True, nullable=False)
+  title = db.Column(db.String(80), nullable=False)
+  content = db.Column(db.String(120), nullable=False)
 
 db.create_all()
-
 
 @app.route('/items', methods=['GET'])
 def get_items():
